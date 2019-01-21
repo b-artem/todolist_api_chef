efc5ba051e45bdc75049fe80bac56fe201c1cf29
b-artem <artemb.dev@gmail.com>
2019-01-22 00:57:47 +0200

    node changes, add production data bag

--- deploy/environments/production.rb
+++ deploy/environments/production.rb
@@ -2,5 +2,5 @@ name 'production'
 description 'Production environment'
 
 default_attributes(
-  domain: 'ec2-18-213-118-145.compute-1.amazonaws.com'
+  domain: 'ec2-35-175-117-76.compute-1.amazonaws.com'
 )
--- deploy/site-cookbooks/app-ruby/recipes/gpg2.rb
+++ deploy/site-cookbooks/app-ruby/recipes/gpg2.rb
@@ -11,15 +11,8 @@ execute 'add gpg2 key' do
     'USER' => user
   })
 
-  # Original command
   command 'command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -'
-
-  # In case of the above command fails try these one instead
-  # command 'gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
-  # command 'sudo gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
-
-  # command 'command curl -sSL https://get.rvm.io | bash -s stable'
-  # command 'command curl -sSL https://get.rvm.io | bash'
+  command 'command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -'
 end
 
 execute 'chown ~/.gnupg' do
